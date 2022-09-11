d5a29a616a4ed24b7844a5589d33f8e193c5bd3e
nedol <nedooleg@gmail.com>
2022-09-11 18:16:17 +0200

    iframe

--- src/routes/user/iframe.svelte
+++ src/routes/user/iframe.svelte
@@ -1,22 +1,6 @@
-<script context="module">
-
-    // export const load = async ({params, fetch, session, stuff})=>{
 
 
-    //     const url = `./api`
-    //     const res = await fetch(url);   
-    //     const data = await res.json();
-    //     console.log(data)
-    //     return {
-    //         props:{data}
-    //     }
-    // }
-    
-</script>
-
-<title>Kolmit User</title>
-
-<link rel='icon' type='image/png' href='/favicon.png'>
+<!-- <link rel='icon' type='image/png' href='/favicon.png'> -->
 <link rel='stylesheet' href='./global.css'>
 <link rel='stylesheet' href='./user.css'>
 
