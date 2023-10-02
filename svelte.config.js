fae3410f27704f330dc1d52007895379adbcae06
kolobot <nedol@narod.ru>
2023-10-02 06:53:42 +0200

    prerender rem

--- svelte.config.js
+++ svelte.config.js
@@ -7,21 +7,21 @@ import adapter from '@sveltejs/adapter-vercel';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-		prerender: {
-			// prerender: {
-			//     // This can be false if you're using a fallback (i.e. SPA mode)
-			// default: false
-			// }
-			handleHttpError: ({ path, referrer, message }) => {
-				// ignore deliberate link to shiny 404 page
-				// if (path === '/not-found' && referrer === '/site') {
-				// 	return;
-				// }
-				// otherwise fail the build
-				throw new Error(message);
-			}
-		}
+		adapter: adapter()
+		// prerender: {
+		// 	// prerender: {
+		// 	//     // This can be false if you're using a fallback (i.e. SPA mode)
+		// 	// default: false
+		// 	// }
+		// 	handleHttpError: ({ path, referrer, message }) => {
+		// 		// ignore deliberate link to shiny 404 page
+		// 		// if (path === '/not-found' && referrer === '/site') {
+		// 		// 	return;
+		// 		// }
+		// 		// otherwise fail the build
+		// 		throw new Error(message);
+		// 	}
+		// }
 		// serviceWorker: {
 		// 	register: false
 		// }
