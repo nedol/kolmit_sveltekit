4f7ba27733852ade605af2e6e55bf552c084446c
kolobot <nedol@narod.ru>
2023-10-03 15:14:15 +0200

    test0

--- .svelte-kit/generated/client/app.js
+++ .svelte-kit/generated/client/app.js
@@ -5,14 +5,17 @@ export const nodes = [
 	() => import('./nodes/1'),
 	() => import('./nodes/2'),
 	() => import('./nodes/3'),
-	() => import('./nodes/4')
+	() => import('./nodes/4'),
+	() => import('./nodes/5')
 ];
 
 export const server_loads = [];
 
 export const dictionary = {
+		"/": [2],
 		"/about": [3],
-		"/site": [4]
+		"/sverdle": [~4],
+		"/sverdle/how-to-play": [5]
 	};
 
 export const hooks = {
--- .svelte-kit/generated/client/nodes/2.js
+++ .svelte-kit/generated/client/nodes/2.js
@@ -1,1 +1,3 @@
-export { default as component } from "../../../../src/routes/operator/lesson/+layout.svelte";
+import * as universal from "../../../../src/routes/+page.js";
+export { universal };
+export { default as component } from "../../../../src/routes/+page.svelte";
