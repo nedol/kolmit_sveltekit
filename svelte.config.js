dc952a3fe47847e0bc8f39a0d55539d4689485b8
kolobot <nedol@narod.ru>
2023-10-16 11:10:52 +0200

    2

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 // import adapter from '@sveltejs/adapter-auto';
-// import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-netlify';
 // import adapter from '@sveltejs/adapter-vercel';
-import adapter from 'svelte-adapter-firebase';
+// import adapter from 'svelte-adapter-firebase';
 
 // import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
