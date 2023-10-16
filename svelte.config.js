3495d126542ba719b86c9186b9eda8b746058535
kolobot <nedol@narod.ru>
2023-10-17 00:29:01 +0200

    fallback

--- svelte.config.js
+++ svelte.config.js
@@ -15,13 +15,9 @@ const config = {
 	}),
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
+			adapter: adapter({
+				fallback: '200.html' // may differ from host to host
+			})
 		}),
 		csrf: {
 			checkOrigin: false
