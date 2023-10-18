aafa6e580fec3e0202e34c21828d64394a56959f
kolobot <nedol@narod.ru>
2023-10-18 22:56:27 +0200

    static2

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
 // import adapter from '@sveltejs/adapter-auto';
-// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static';
 // import adapter from '@sveltejs/adapter-netlify';
 // import adapter from '@sveltejs/adapter-vercel';
-import adapter from '@sveltejs/adapter-node';
+// import adapter from '@sveltejs/adapter-node';
 // import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
