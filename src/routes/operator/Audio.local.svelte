799956ea2440ffcca833d6299b7b60163e8a5675
kolobot <nedol@narod.ru>
2023-12-10 18:24:16 +0100

    call ring

--- src/routes/operator/Audio.local.svelte
+++ src/routes/operator/Audio.local.svelte
@@ -1,39 +1,18 @@
 <script>
 	import { onMount } from 'svelte';
-	import { ring } from '../assets/mp3/ring';
 
 	export let paused = true;
 	let ls;
-	let src;
 	onMount(async () => {
-		// ls = document.getElementById('localSound');
+		ls = document.getElementById('localSound');
 	});
 
 	$: if (ls) {
 		if (paused) ls.pause();
-		else {
-			ls.src = ring.data;
-			// ls.srcObject = ring.data;
-			let playPromise = ls.play();
-			if (playPromise !== undefined) {
-				playPromise
-					.then((_) => {
-						// Automatic playback started!
-						// Show playing UI.
-					})
-					.catch((error) => {
-						// Auto-play was prevented
-						// Show paused UI.
-					});
-			}
-		}
+		else ls.play();
 	}
 </script>
 
-<audio bind:this={ls}>
-	<!-- <track kind="captions" /> -->
-</audio>
-
-<!-- <audio id="localSound" src="../src/routes/assets/mp3/ring.mp3">
+<audio id="localSound" src="../src/routes/assets/mp3/ring.mp3">
 	<track kind="captions" />
-</audio> -->
+</audio>
--- src/routes/user/Audio.local.svelte
+++ src/routes/user/Audio.local.svelte
@@ -1,39 +1,20 @@
 <script>
 	import { onMount } from 'svelte';
-	import { call } from '../assets/mp3/call';
 
 	export let paused = true;
 	let ls;
-	let src;
 	onMount(async () => {
-		// ls = document.getElementById('localSound');
+		ls = document.getElementById('localSound');
 	});
 
 	$: if (ls) {
 		if (paused) ls.pause();
 		else {
-			ls.src = call.data;
-			// ls.srcObject = call.data;
-			let playPromise = ls.play();
-			if (playPromise !== undefined) {
-				playPromise
-					.then((_) => {
-						// Automatic playback started!
-						// Show playing UI.
-					})
-					.catch((error) => {
-						// Auto-play was prevented
-						// Show paused UI.
-					});
-			}
+			ls.play();
 		}
 	}
 </script>
 
-<audio bind:this={ls} autoplay>
-	<!-- <track kind="captions" /> -->
-</audio>
-
-<!-- <audio id="localSound" autoplay src="../src/routes/assets/mp3/call.mp3">
+<audio id="localSound" autoplay src="./assets/mp3/call.mp3">
 	<track kind="captions" />
-</audio> -->
+</audio>
