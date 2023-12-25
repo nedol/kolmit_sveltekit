86c5c60733d5b527a425d2000e981586116c7135
kolobot <nedol@narod.ru>
2023-12-25 11:55:44 +0100

    wonen

---
+++ src/routes/assets/dialog/Verhuisd.Dialog.2.js
@@ -0,0 +1,233 @@
+export let dialog_data = {
+	title: 'Intro',
+	content: [
+		{
+			num: '1',
+			question: {
+				nl: 'Wat kan Sam ’s avonds doen?',
+				ru: 'Что Сэм может делать вечером?',
+				uk: 'Що Сем може робити ввечері?',
+				fr: 'Que peut faire Sam le soir?',
+				en: 'What can Sam do in the evening?',
+				de: 'Was kann Sam abends tun?'
+			},
+			answer: {
+				nl: 'Sam kan ’s avonds de dozen uitpakken.',
+				ru: 'Сэм может распаковывать коробки вечером.',
+				uk: 'Сем може розпаковувати коробки ввечері.',
+				fr: 'Sam peut déballer des boîtes le soir.',
+				en: 'Sam can unpack boxes in the evening.',
+				de: 'Sam kann abends die Kisten auspacken.'
+			}
+		},
+		{
+			num: '2',
+			question: {
+				nl: 'Wat moet Simon ‘s avonds doen?',
+				ru: 'Что Саймон должен делать вечером?',
+				uk: 'Що Саймон повинен робити ввечері?',
+				fr: 'Que doit faire Simon le soir?',
+				en: 'What does Simon have to do in the evening?',
+				de: 'Was muss Simon abends tun?'
+			},
+			answer: {
+				nl: 'Simon moet ’s avonds de gordijnen ophangen.',
+				ru: 'Саймон должен вечером повесить шторы.',
+				uk: 'Саймон повинен ввечері підвісити штори.',
+				fr: 'Simon doit accrocher les rideaux le soir.',
+				en: 'Simon has to hang the curtains in the evening.',
+				de: 'Simon muss abends die Vorhänge aufhängen.'
+			}
+		},
+		{
+			num: '3',
+			question: {
+				nl: 'Wat wil Pauline in de namiddag doen?',
+				ru: 'Что хочет Полин в послеполуденный период?',
+				uk: 'Що хоче Полін вдень?',
+				fr: "Que veut faire Pauline dans l'après-midi?",
+				en: 'What does Pauline want to do in the afternoon?',
+				de: 'Was möchte Pauline am Nachmittag tun?'
+			},
+			answer: {
+				nl: 'Pauline wil in de namiddag de meubels monteren.',
+				ru: 'Полин хочет собрать мебель днем.',
+				uk: 'Полін хоче збирати меблі вдень.',
+				fr: "Pauline veut monter les meubles dans l'après-midi.",
+				en: 'Pauline wants to assemble the furniture in the afternoon.',
+				de: 'Pauline möchte am Nachmittag die Möbel montieren.'
+			}
+		},
+		{
+			num: '4',
+			question: {
+				nl: 'Wat moet Marc in de namiddag doen?',
+				ru: 'Что должен делать Марк днем?',
+				uk: 'Що повинен робити Марк вдень?',
+				fr: "Que doit faire Marc dans l'après-midi?",
+				en: 'What does Marc have to do in the afternoon?',
+				de: 'Was muss Marc am Nachmittag tun?'
+			},
+			answer: {
+				nl: 'Marc moet in de namiddag het zware materiaal verplaatsen.',
+				ru: 'Марк должен перемещать тяжелый материал днем.',
+				uk: 'Марк повинен переміщувати важкий матеріал вдень.',
+				fr: "Marc doit déplacer le matériel lourd dans l'après-midi.",
+				en: 'Marc has to move the heavy equipment in the afternoon.',
+				de: 'Marc muss am Nachmittag das schwere Material verschieben.'
+			}
+		},
+		{
+			num: '5',
+			question: {
+				nl: 'Wat wil Amelia ’s avonds doen?',
+				ru: 'Что хочет Амелия делать вечером?',
+				uk: 'Що хоче Емілія робити ввечері?',
+				fr: 'Que veut faire Amelia le soir?',
+				en: 'What does Amelia want to do in the evening?',
+				de: 'Was möchte Amelia abends tun?'
+			},
+			answer: {
+				nl: 'Amelia wil ’s avonds in het vorige huis poetsen.',
+				ru: 'Амелия хочет вечером убирать в предыдущем доме.',
+				uk: 'Емілія хоче ввечері прибирати в попередньому будинку.',
+				fr: 'Amelia veut nettoyer la maison précédente le soir.',
+				en: 'Amelia wants to clean the previous house in the evening.',
+				de: 'Amelia möchte abends im vorherigen Haus putzen.'
+			}
+		},
+		{
+			num: '6',
+			question: {
+				nl: 'Wat kan Pauline in de voormiddag doen?',
+				ru: 'Что Полин может делать утром?',
+				uk: 'Що Полін може робити вранці?',
+				fr: 'Que peut faire Pauline le matin?',
+				en: 'What can Pauline do in the morning?',
+				de: 'Was kann Pauline am Vormittag tun?'
+			},
+			answer: {
+				nl: 'Pauline kan in de voormiddag de lunch klaarmaken.',
+				ru: 'Полин может приготовить обед утром.',
+				uk: 'Полін може приготувати обід вранці.',
+				fr: 'Pauline peut préparer le déjeuner le matin.',
+				en: 'Pauline can prepare lunch in the morning.',
+				de: 'Pauline kann am Vormittag das Mittagessen zubereiten.'
+			}
+		},
+		{
+			num: '7',
+			question: {
+				nl: 'Wat wil Dave ’s avonds doen?',
+				ru: 'Что хочет Дейв делать вечером?',
+				uk: 'Що хоче Дейв робити ввечері?',
+				fr: 'Que veut faire Dave le soir?',
+				en: 'What does Dave want to do in the evening?',
+				de: 'Was möchte Dave abends tun?'
+			},
+			answer: {
+				nl: 'Dave wil ’s avonds voor iedereen pizza halen.',
+				ru: 'Дейв хочет вечером купить пиццу для всех.',
+				uk: 'Дейв хоче ввечері купити піцу для всіх.',
+				fr: 'Dave veut acheter de la pizza pour tout le monde le soir.',
+				en: 'Dave wants to get pizza for everyone in the evening.',
+				de: 'Dave möchte abends Pizza für alle holen.'
+			}
+		},
+		{
+			num: '8',
+			question: {
+				nl: "Wat moeten Bart en Ricky 's avonds doen?",
+				ru: 'Что должны делать Барт и Рики вечером?',
+				uk: 'Що повинні робити Барт і Ріккі ввечері?',
+				fr: 'Que doivent faire Bart et Ricky le soir?',
+				en: 'What do Bart and Ricky have to do in the evening?',
+				de: 'Was müssen Bart und Ricky abends tun?'
+			},
+			answer: {
+				nl: 'Bart en Ricky moeten ’s avonds de tv en het internet aansluiten.',
+				ru: 'Барт и Рики должны подключить телевизор и интернет вечером.',
+				uk: 'Барт і Ріккі повинні підключити телевізор і інтернет ввечері.',
+				fr: "Bart et Ricky doivent connecter la télévision et l'internet le soir.",
+				en: 'Bart and Ricky have to connect the TV and the internet in the evening.',
+				de: 'Bart und Ricky müssen abends den Fernseher und das Internet anschließen.'
+			}
+		},
+		{
+			num: '9',
+			question: {
+				nl: 'Wat kan Marc ’s avonds doen?',
+				ru: 'Что Марк может делать вечером?',
+				uk: 'Що Марк може робити ввечері?',
+				fr: 'Que peut faire Marc le soir?',
+				en: 'What can Marc do in the evening?',
+				de: 'Was kann Marc abends tun?'
+			},
+			answer: {
+				nl: 'Marc kan ’s avonds alles opruimen.',
+				ru: 'Марк может убирать все вечером.',
+				uk: 'Марк може прибирати все ввечері.',
+				fr: 'Marc peut tout ranger le soir.',
+				en: 'Marc can tidy up everything in the evening.',
+				de: 'Marc kann abends alles aufräumen.'
+			}
+		},
+		{
+			num: '10',
+			question: {
+				nl: 'Wat moeten Nina en Pippa in de namiddag doen?',
+				ru: 'Что должны делать Нина и Пиппа днем?',
+				uk: 'Що повинні робити Ніна і Піппа вдень?',
+				fr: "Que doivent faire Nina et Pippa dans l'après-midi?",
+				en: 'What do Nina and Pippa have to do in the afternoon?',
+				de: 'Was müssen Nina und Pippa am Nachmittag tun?'
+			},
+			answer: {
+				nl: 'Nina en Pippa moeten in de namiddag de meubels monteren.',
+				ru: 'Нина и Пиппа должны собирать мебель днем.',
+				uk: 'Ніна і Піппа повинні збирати меблі вдень.',
+				fr: "Nina et Pippa doivent monter les meubles dans l'après-midi.",
+				en: 'Nina and Pippa have to assemble the furniture in the afternoon.',
+				de: 'Nina und Pippa müssen am Nachmittag die Möbel montieren.'
+			}
+		},
+		{
+			num: '11',
+			question: {
+				nl: 'Wat moet Sam in de namiddag doen?',
+				ru: 'Что Сэм должен делать днем?',
+				uk: 'Що Сем повинен робити вдень?',
+				fr: "Que doit faire Sam dans l'après-midi?",
+				en: 'What does Sam have to do in the afternoon?',
+				de: 'Was muss Sam am Nachmittag tun?'
+			},
+			answer: {
+				nl: 'Sam moet in de namiddag de apparaten installeren.',
+				ru: 'Сэм должен устанавливать устройства днем.',
+				uk: 'Сем повинен вдень встановлювати пристрої.',
+				fr: "Sam doit installer les appareils dans l'après-midi.",
+				en: 'Sam has to install the devices in the afternoon.',
+				de: 'Sam muss am Nachmittag die Geräte installieren.'
+			}
+		},
+		{
+			num: '12',
+			question: {
+				nl: 'Wat kan Amelia in de namiddag doen?',
+				ru: 'Что может делать Амелия днем?',
+				uk: 'Що може робити Емілія вдень?',
+				fr: "Que peut faire Amelia dans l'après-midi?",
+				en: 'What can Amelia do in the afternoon?',
+				de: 'Was kann Amelia am Nachmittag tun?'
+			},
+			answer: {
+				nl: 'Amelia kan in de namiddag boodschappen doen.',
+				ru: 'Амелия может делать покупки днем.',
+				uk: 'Емілія може робити покупки вдень.',
+				fr: "Amelia peut faire des courses dans l'après-midi.",
+				en: 'Amelia can go grocery shopping in the afternoon.',
+				de: 'Amelia kann am Nachmittag einkaufen gehen.'
+			}
+		}
+	]
+};
---
+++ src/routes/assets/dialog/Wonen.Dialog.js
@@ -0,0 +1,348 @@
+export let dialog_data = {
+	title: 'Intro',
+	content: [
+		{
+			num: '1',
+			question: {
+				nl: 'Woon je in een huis of in een appartement?',
+				ru: 'Ты живешь в доме или в квартире?',
+				uk: 'Ти живеш у будинку чи в квартирі?',
+				fr: 'Vives-tu dans une maison ou dans un appartement?',
+				en: 'Do you live in a house or an apartment?',
+				de: 'Wohnst du in einem Haus oder in einer Wohnung?'
+			},
+			answer: {
+				nl: 'Ik woon in een appartement.',
+				ru: 'Я живу в квартире.',
+				uk: 'Я живу в квартирі.',
+				fr: 'Je vis dans un appartement.',
+				en: 'I live in an apartment.',
+				de: 'Ich wohne in einer Wohnung.'
+			}
+		},
+		{
+			num: '2',
+			question: {
+				nl: 'Op welke verdieping ligt het appartement?',
+				ru: 'На каком этаже находится квартира?',
+				uk: 'На якому поверсі розташована квартира?',
+				fr: "À quel étage se trouve l'appartement?",
+				en: 'On which floor is the apartment located?',
+				de: 'Auf welchem Stockwerk befindet sich die Wohnung?'
+			},
+			answer: {
+				nl: 'Het appartement ligt op de derde verdieping.',
+				ru: 'Квартира находится на третьем этаже.',
+				uk: 'Квартира розташована на третьому поверсі.',
+				fr: "L'appartement est au troisième étage.",
+				en: 'The apartment is on the third floor.',
+				de: 'Die Wohnung befindet sich im dritten Stock.'
+			}
+		},
+		{
+			num: '3',
+			question: {
+				nl: 'Welke toestellen zijn er in de keuken?',
+				ru: 'Какие устройства в кухне?',
+				uk: 'Які пристрої є на кухні?',
+				fr: 'Quels appareils y a-t-il dans la cuisine?',
+				en: 'Which appliances are there in the kitchen?',
+				de: 'Welche Geräte gibt es in der Küche?'
+			},
+			answer: {
+				nl: 'Er is een koelkast, een diepvriezer, een oven en een microgolfoven.',
+				ru: 'Есть холодильник, морозильник, духовка и микроволновка.',
+				uk: 'Є холодильник, морозильник, духовка і мікрохвильовка.',
+				fr: 'Il y a un réfrigérateur, un congélateur, un four et un micro-ondes.',
+				en: 'There is a refrigerator, a freezer, an oven, and a microwave.',
+				de: 'Es gibt einen Kühlschrank, einen Gefrierschrank, einen Backofen und eine Mikrowelle.'
+			}
+		},
+		{
+			num: '4',
+			question: {
+				nl: 'Is er een aparte wc?',
+				ru: 'Есть отдельный туалет?',
+				uk: 'Чи є окремий туалет?',
+				fr: 'Y a-t-il des toilettes séparées?',
+				en: 'Is there a separate toilet?',
+				de: 'Gibt es eine separate Toilette?'
+			},
+			answer: {
+				nl: 'Nee, er is geen aparte wc, maar er is een wc in de badkamer.',
+				ru: 'Нет, отдельного туалета нет, но есть туалет в ванной комнате.',
+				uk: 'Ні, окремого туалету немає, але є туалет у ванній кімнаті.',
+				fr: "Non, il n'y a pas de toilettes séparées, mais il y a des toilettes dans la salle de bains.",
+				en: 'No, there is no separate toilet, but there is a toilet in the bathroom.',
+				de: 'Nein, es gibt keine separate Toilette, aber es gibt eine Toilette im Badezimmer.'
+			}
+		},
+		{
+			num: '5',
+			question: {
+				nl: 'Hoe ziet de badkamer eruit?',
+				ru: 'Как выглядит ванная комната?',
+				uk: 'Як виглядає ванна кімната?',
+				fr: 'À quoi ressemble la salle de bains?',
+				en: 'What does the bathroom look like?',
+				de: 'Wie sieht das Badezimmer aus?'
+			},
+			answer: {
+				nl: 'Er is een bad, een douche, een spiegel en een wastafel.',
+				ru: 'Есть ванна, душ, зеркало и умывальник.',
+				uk: 'Є ванна, душ, дзеркало і умивальник.',
+				fr: 'Il y a une baignoire, une douche, un miroir et un lavabo.',
+				en: 'There is a bath, a shower, a mirror, and a sink.',
+				de: 'Es gibt eine Badewanne, eine Dusche, einen Spiegel und ein Waschbecken.'
+			}
+		},
+		{
+			num: '6',
+			question: {
+				nl: 'Hoeveel bedraagt de waarborg?',
+				ru: 'Какова сумма залога?',
+				uk: 'Яка сума застави?',
+				fr: 'Quel est le montant de la caution?',
+				en: 'What is the amount of the deposit?',
+				de: 'Wie hoch ist die Kaution?'
+			},
+			answer: {
+				nl: 'De waarborg bedraagt 2 maanden huur.',
+				ru: 'Сумма залога составляет 2 месяца аренды.',
+				uk: 'Сума застави складає 2 місяці оренди.',
+				fr: 'Le montant de la caution est de 2 mois de loyer.',
+				en: "The deposit amount is 2 months' rent.",
+				de: 'Die Kaution beträgt 2 Monatsmieten.'
+			}
+		},
+		{
+			num: '7',
+			question: {
+				nl: 'Hoeveel slaapkamers zijn er?',
+				ru: 'Сколько спален?',
+				uk: 'Скільки спалень?',
+				fr: 'Combien de chambres y a-t-il?',
+				en: 'How many bedrooms are there?',
+				de: 'Wie viele Schlafzimmer gibt es?'
+			},
+			answer: {
+				nl: 'Er is één grote slaapkamer.',
+				ru: 'Есть одна большая спальня.',
+				uk: 'Є одна велика спальня.',
+				fr: 'Il y a une grande chambre.',
+				en: 'There is one large bedroom.',
+				de: 'Es gibt ein großes Schlafzimmer.'
+			}
+		},
+		{
+			num: '8',
+			question: {
+				nl: 'In welke straat ligt het appartement?',
+				ru: 'На какой улице находится квартира?',
+				uk: 'На якій вулиці розташована квартира?',
+				fr: "Dans quelle rue se trouve l'appartement?",
+				en: 'In which street is the apartment located?',
+				de: 'In welcher Straße befindet sich die Wohnung?'
+			},
+			answer: {
+				nl: 'Het appartement ligt in de Kuiperstraat.',
+				ru: 'Квартира находится на улице Кёйпер.',
+				uk: 'Квартира розташована на вулиці Кюйпер.',
+				fr: "L'appartement est situé dans la rue Kuiperstraat.",
+				en: 'The apartment is located in Kuiperstraat.',
+				de: 'Die Wohnung befindet sich in der Kuiperstraat.'
+			}
+		},
+		{
+			num: '9',
+			question: {
+				nl: 'Is er een kelder?',
+				ru: 'Есть ли подвал?',
+				uk: 'Чи є підвал?',
+				fr: 'Y a-t-il une cave?',
+				en: 'Is there a basement?',
+				de: 'Gibt es einen Keller?'
+			},
+			answer: {
+				nl: 'Ja, er is een kelder.',
+				ru: 'Да, есть подвал.',
+				uk: 'Так, є підвал.',
+				fr: 'Oui, il y a une cave.',
+				en: 'Yes, there is a basement.',
+				de: 'Ja, es gibt einen Keller.'
+			}
+		},
+		{
+			num: '10',
+			question: {
+				nl: 'Zijn er nog extra kosten?',
+				ru: 'Есть ли дополнительные расходы?',
+				uk: 'Чи є додаткові витрати?',
+				fr: 'Y a-t-il des frais supplémentaires?',
+				en: 'Are there any additional costs?',
+				de: 'Gibt es zusätzliche Kosten?'
+			},
+			answer: {
+				nl: 'Nee, er zijn geen extra kosten.',
+				ru: 'Нет, дополнительных расходов нет.',
+				uk: 'Ні, додаткових витрат немає.',
+				fr: "Non, il n'y a pas de frais supplémentaires.",
+				en: 'No, there are no additional costs.',
+				de: 'Nein, es gibt keine zusätzlichen Kosten.'
+			}
+		},
+
+		{
+			num: '11',
+			question: {
+				nl: 'Woon je in een appartement?',
+				ru: 'Ты живешь в квартире?',
+				uk: 'Ти живеш в квартирі?',
+				fr: 'Vivez-vous dans un appartement?',
+				en: 'Do you live in an apartment?',
+				de: 'Wohnst du in einer Wohnung?'
+			},
+			answer: {
+				nl: 'Nee, ik woon in een huis. Alles is (in de huur) inbegrepen.',
+				ru: 'Нет, я живу в доме. Все включено (в аренду).',
+				uk: 'Ні, я живу в будинку. Все включено (у оренду).',
+				fr: 'Non, je vis dans une maison. Tout est inclus (dans le loyer).',
+				en: 'No, I live in a house. Everything is included (in the rent).',
+				de: 'Nein, ich wohne in einem Haus. Alles ist (in der Miete) inbegriffen.'
+			}
+		},
+		{
+			num: '12',
+			question: {
+				nl: 'In welke straat staat het huis?',
+				ru: 'На какой улице находится дом?',
+				uk: 'На якій вулиці розташований будинок?',
+				fr: 'Dans quelle rue se trouve la maison?',
+				en: 'In which street is the house located?',
+				de: 'In welcher Straße steht das Haus?'
+			},
+			answer: {
+				nl: 'Het huis staat in de Volksstraat.',
+				ru: 'Дом находится на улице Фолкс.',
+				uk: 'Будинок розташований на вулиці Волкс.',
+				fr: 'La maison est située dans la rue Volks.',
+				en: 'The house is located in Volksstraat.',
+				de: 'Das Haus befindet sich in der Volksstraat.'
+			}
+		},
+		{
+			num: '13',
+			question: {
+				nl: 'Hoeveel verdiepingen zijn er?',
+				ru: 'Сколько этажей там?',
+				uk: 'Скільки поверхів там?',
+				fr: "Combien d'étages y a-t-il?",
+				en: 'How many floors are there?',
+				de: 'Wie viele Stockwerke gibt es?'
+			},
+			answer: {
+				nl: 'Er is één verdieping.',
+				ru: 'Там один этаж.',
+				uk: 'Там один поверх.',
+				fr: 'Il y a un étage.',
+				en: 'There is one floor.',
+				de: 'Es gibt eine Etage.'
+			}
+		},
+		{
+			num: '14',
+			question: {
+				nl: 'Is er een grote tuin?',
+				ru: 'Есть большой сад?',
+				uk: 'Чи є великий сад?',
+				fr: 'Y a-t-il un grand jardin?',
+				en: 'Is there a big garden?',
+				de: 'Gibt es einen großen Garten?'
+			},
+			answer: {
+				nl: 'Nee, er is geen grote tuin. Er is een kleine tuin.',
+				ru: 'Нет, нет большого сада. Есть небольшой сад.',
+				uk: 'Ні, немає великого саду. Є невеликий сад.',
+				fr: "Non, il n'y a pas de grand jardin. Il y a un petit jardin.",
+				en: 'No, there is no big garden. There is a small garden.',
+				de: 'Nein, es gibt keinen großen Garten. Es gibt einen kleinen Garten.'
+			}
+		},
+		{
+			num: '15',
+			question: {
+				nl: 'Hoe ziet de keuken eruit?',
+				ru: 'Как выглядит кухня?',
+				uk: 'Як виглядає кухня?',
+				fr: 'À quoi ressemble la cuisine?',
+				en: 'What does the kitchen look like?',
+				de: 'Wie sieht die Küche aus?'
+			},
+			answer: {
+				nl: 'Er zijn kasten, er is een koelkast en er is een afwasmachine.',
+				ru: 'Там есть шкафы, есть холодильник и есть посудомоечная машина.',
+				uk: 'Там є шафи, є холодильник і є посудомийна машина.',
+				fr: 'Il y a des armoires, il y a un réfrigérateur et il y a un lave-vaisselle.',
+				en: 'There are cabinets, there is a refrigerator, and there is a dishwasher.',
+				de: 'Es gibt Schränke, es gibt einen Kühlschrank und es gibt eine Spülmaschine.'
+			}
+		},
+		{
+			num: '16',
+			question: {
+				nl: 'Is er centrale verwarming?',
+				ru: 'Есть центральное отопление?',
+				uk: 'Чи є центральне опалення?',
+				fr: 'Y a-t-il du chauffage central?',
+				en: 'Is there central heating?',
+				de: 'Gibt es Zentralheizung?'
+			},
+			answer: {
+				nl: 'Ja, er is centrale verwarming.',
+				ru: 'Да, есть центральное отопление.',
+				uk: 'Так, є центральне опалення.',
+				fr: 'Oui, il y a du chauffage central.',
+				en: 'Yes, there is central heating.',
+				de: 'Ja, es gibt Zentralheizung.'
+			}
+		},
+		{
+			num: '17',
+			question: {
+				nl: 'Hoeveel slaapkamers zijn er?',
+				ru: 'Сколько там спален?',
+				uk: 'Скільки там спалень?',
+				fr: 'Combien de chambres y a-t-il?',
+				en: 'How many bedrooms are there?',
+				de: 'Wie viele Schlafzimmer gibt es?'
+			},
+			answer: {
+				nl: 'Er zijn drie slaapkamers.',
+				ru: 'Там три спальни.',
+				uk: 'Там три спальні.',
+				fr: 'Il y a trois chambres.',
+				en: 'There are three bedrooms.',
+				de: 'Es gibt drei Schlafzimmer.'
+			}
+		},
+		{
+			num: '18',
+			question: {
+				nl: 'Op welke verdieping ligt het appartement?',
+				ru: 'На каком этаже находится квартира?',
+				uk: 'На якому поверсі розташована квартира?',
+				fr: "À quel étage se trouve l'appartement?",
+				en: 'On which floor is the apartment located?',
+				de: 'Auf welcher Etage befindet sich die Wohnung?'
+			},
+			answer: {
+				nl: 'Het appartement ligt op het gelijkvloers.',
+				ru: 'Квартира находится на первом этаже.',
+				uk: 'Квартира розташована на першому поверсі.',
+				fr: "L'appartement est situé au rez-de-chaussée.",
+				en: 'The apartment is located on the ground floor.',
+				de: 'Die Wohnung befindet sich im Erdgeschoss.'
+			}
+		}
+	]
+};
