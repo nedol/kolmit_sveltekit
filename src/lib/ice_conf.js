32905b14b3efe7d714ca3c519538c9f3ff07df7c
kolobot <nedol@narod.ru>
2024-01-02 21:41:23 +0100

    turn

--- src/lib/ice_conf.js
+++ src/lib/ice_conf.js
@@ -1,6 +1,7 @@
 export let ice_conf = {
 	stun: {
 		urls: [
+			'stun:stun.relay.metered.ca:80',
 			'stun:stun1.l.google.com:19302',
 			'stun:stun2.l.google.com:19302',
 			'stun:stun3.l.google.com:19302',
@@ -8,9 +9,14 @@ export let ice_conf = {
 		]
 	},
 	turn: {
-		urls: 'turn:openrelay.metered.ca:80',
-		username: 'openrelayproject',
-		credentials: 'openrelayproject'
+		urls: [
+			'turn:standard.relay.metered.ca:80',
+			'turn:standard.relay.metered.ca:80?transport=tcp',
+			'turn:standard.relay.metered.ca:443',
+			'turn:standard.relay.metered.ca:443?transport=tcp'
+		],
+		username: '077cf3ec6ba3c88b0fc27137',
+		credential: 'Veamdb+7k9Zf/C/W'
 	},
 	lifetimeDuration: '86400s'
 };
