fe2f9459387e7e77869cf38c73047a1177830fd0
kolobot <nedol@narod.ru>
2024-01-04 23:09:07 +0100

    expressturn

--- src/lib/ice_conf.0.js
+++ src/lib/ice_conf.0.js
@@ -7,9 +7,9 @@ export let ice_conf = {
 	],
 	turn: [
 		{
-			urls: 'turn:relay1.expressturn.com:3478?transport=tcp',
-			username: 'ef5N75GPIIOV97AS3J',
-			credential: 'sIWl6OqxV67ZogkM'
+			urls: 'turn:0.0.0.0:3478?transport=tcp',
+			username: 'username',
+			credential: 'password'
 		}
 	],
 	lifetimeDuration: '86400s'
