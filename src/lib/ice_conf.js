b959d5b02383b565a74bff1af958975de1d3cf29
kolobot <nedol@narod.ru>
2024-01-04 17:09:32 +0100

    expressturn

--- src/lib/ice_conf.js
+++ src/lib/ice_conf.js
@@ -26,6 +26,11 @@ export let ice_conf = {
 			urls: 'turn:standard.relay.metered.ca:443?transport=tcp',
 			username: '077cf3ec6ba3c88b0fc27137',
 			credential: 'Veamdb+7k9Zf/C/W'
+		},
+		{
+			urls: 'turn:relay1.expressturn.com:3478',
+			username: 'ef5N75GPIIOV97AS3J',
+			credential: 'sIWl6OqxV67ZogkM'
 		}
 	],
 	lifetimeDuration: '86400s'
