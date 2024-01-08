196f8a855f99b6a41d0d8aac69f98473df5545a1
kolobot <nedol@narod.ru>
2024-01-08 17:52:13 +0100

    vakantie

---
+++ src/routes/assets/dialog/Op vakantie.Barcelona.js
@@ -0,0 +1,95 @@
+export let dialog_data = {
+	title: 'Intro',
+	content: [
+		{
+			question: {
+				nl: 'Wat hebben Amir en zijn gezin vorige zomer gedaan?',
+				ru: 'Что делала семья Амира прошлым летом?',
+				uk: 'Що робила родина Аміра минулого літа?',
+				fr: "Que faisaient Amir et sa famille l'été dernier?",
+				en: 'What did Amir and his family do last summer?',
+				de: 'Was haben Amir und seine Familie letzten Sommer gemacht?'
+			},
+			answer: {
+				nl: "Vorige zomer bezocht Amir met zijn gezin Barcelona. Ze vlogen 's ochtends met het vliegtuig.",
+				ru: 'Летом прошлого года Амир с семьей посетили Барселону. Они летели туда утром на самолете.',
+				uk: "Минулого літа Амір і його сім'я відвідали Барселону. Вони летіли туди зранку літаком.",
+				fr: "L'été dernier, Amir et sa famille ont visité Barcelone. Ils ont pris l'avion le matin.",
+				en: 'Last summer, Amir and his family visited Barcelona. They flew there in the morning by plane.',
+				de: 'Letzten Sommer besuchte Amir mit seiner Familie Barcelona. Sie flogen morgens mit dem Flugzeug.'
+			}
+		},
+		{
+			question: {
+				nl: 'Wat deden ze op de eerste dag van hun vakantie?',
+				ru: 'Что они делали в первый день отпуска?',
+				uk: 'Що вони робили в перший день відпустки?',
+				fr: 'Que faisaient-ils le premier jour de leurs vacances?',
+				en: 'What did they do on the first day of their vacation?',
+				de: 'Was haben sie am ersten Tag ihres Urlaubs gemacht?'
+			},
+			answer: {
+				nl: "Op de eerste dag van hun vakantie wandelden ze 's avonds naar de naberlijn. Daar genoten ze van tapas en Spaanse wijn.",
+				ru: 'В первый день отпуска вечером они прогулялись по набережной. Там они наслаждались тапас и испанским вином.',
+				uk: 'В перший день відпустки ввечері вони прогулялися по набережні. Там вони насолоджувалися тапас та іспанським вином.',
+				fr: 'Le premier jour de leurs vacances, ils ont marché le soir le long de la promenade. Ils ont dégusté des tapas et du vin espagnol.',
+				en: 'On the first day of their vacation, they walked in the evening to the promenade. There, they enjoyed tapas and Spanish wine.',
+				de: 'Am ersten Tag ihres Urlaubs spazierten sie abends zur Promenade. Dort genossen sie Tapas und spanischen Wein.'
+			}
+		},
+		{
+			question: {
+				nl: 'Wat deden ze op de derde dag van hun vakantie?',
+				ru: 'Что они делали в третий день отпуска?',
+				uk: 'Що вони робили в третій день відпустки?',
+				fr: 'Que faisaient-ils le troisième jour de leurs vacances?',
+				en: 'What did they do on the third day of their vacation?',
+				de: 'Was haben sie am dritten Tag ihres Urlaubs gemacht?'
+			},
+			answer: {
+				nl: 'Op de derde dag maakten ze een uitstap naar het strand. Julie en de kinderen zeilden, terwijl Amir een duikcursus volgde.',
+				ru: 'В третий день они сделали поездку на пляж. Джули и дети парусили, в то время как Амир проходил курс по дайвингу.',
+				uk: 'В третій день вони зробили поїздку на пляж. Джулі і діти вітрильничали, а Амір в той час взяв курс дайвінгу.',
+				fr: "Le troisième jour de leurs vacances, ils ont fait une excursion à la plage. Julie et les enfants ont navigué, tandis qu'Amir suivait un cours de plongée.",
+				en: 'On the third day of their vacation, they made a trip to the beach. Julie and the kids sailed, while Amir took a diving course.',
+				de: 'Am dritten Tag ihres Urlaubs machten sie einen Ausflug zum Strand. Julie und die Kinder segelten, während Amir einen Tauchkurs machte.'
+			}
+		},
+		{
+			question: {
+				nl: 'Hoe eindigde hun laatste dag in Barcelona?',
+				ru: 'Как закончился их последний день в Барселоне?',
+				uk: 'Як закінчився їх останній день в Барселоні?',
+				fr: "Comment leur dernier jour à Barcelone s'est-il terminé?",
+				en: 'How did their last day in Barcelona end?',
+				de: 'Wie endete ihr letzter Tag in Barcelona?'
+			},
+			answer: {
+				nl: "Hun laatste dag begon 's morgens met een busrit naar de Montjuïc-berg. Daar bezochten ze eerst een museum en zagen daarna het olympisch stadion. Na de middag winkelden ze op de Ramblas en 's avonds zwommen ze in het hotelzwembad.",
+				ru: 'Их последний день начался с утренней поездки на автобусе на гору Монтжуик. Там они сначала посетили музей, а затем увидели олимпийский стадион. После полудня они гуляли по Рамблас, а вечером плавали в отеле в бассейне.',
+				uk: 'Їх останній день розпочався з ранкової поїздки на автобусі на гору Монтжуїк. Там вони спочатку відвідали музей, а потім побачили олімпійський стадіон. Після обіду вони гуляли по Рамблас, а ввечері плавали в басейні готелю.',
+				fr: "Leur dernier jour a commencé le matin par un trajet en bus jusqu'à la montagne Montjuïc. Là, ils ont d'abord visité un musée, puis ont vu le stade olympique. Après le déjeuner, ils ont fait du shopping sur les Ramblas et le soir, ils ont nagé dans la piscine de l'hôtel.",
+				en: 'Their last day started in the morning with a bus trip to the Montjuïc mountain. There, they first visited a museum and then saw the Olympic stadium. Afternoon was spent shopping on the Ramblas, and in the evening, they swam in the hotel pool.',
+				de: 'Ihr letzter Tag begann morgens mit einer Busfahrt zum Montjuïc-Berg. Dort besuchten sie zuerst ein Museum und sahen dann das Olympiastadion. Nachmittags bummelten sie auf den Ramblas und abends schwammen sie im Hotelpool.'
+			}
+		},
+		{
+			question: {
+				nl: "Waar gingen ze 's nachts naartoe?",
+				ru: 'Куда они поехали ночью?',
+				uk: 'Куди вони поїхали вночі?',
+				fr: 'Où sont-ils allés la nuit?',
+				en: 'Where did they go at night?',
+				de: 'Wohin sind sie nachts gegangen?'
+			},
+			answer: {
+				nl: "'s Nachts vertrokken ze terug naar België.",
+				ru: 'Ночью они отправились обратно в Бельгию.',
+				uk: 'Вночі вони вирушили назад до Бельгії.',
+				fr: 'Ils sont repartis en Belgique la nuit.',
+				en: 'At night, they departed back to Belgium.',
+				de: 'Nachts sind sie zurück nach Belgien gereist.'
+			}
+		}
+	]
+};
